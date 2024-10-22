180a9f3a463feab20292b7b77bd53d8e6e833918
Rob McKnight <rmcknight451@gmail.com>
2024-10-22 09:54:19 -0400

    Remove stainless sdk

--- src/lsproxy/types/shared/__init__.py
+++
@@ -1,4 +0,0 @@
-# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-from .file_position import FilePosition as FilePosition
-from .symbol_response import SymbolResponse as SymbolResponse
